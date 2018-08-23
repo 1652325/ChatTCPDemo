b2f70205cea28729eb94d5e6b879f23283792b5b
Antoine Toutant <1652325@etu.cegepsth.qc.ca>
2018-08-23 19:48:00 -0400

    Add project files.

---
+++ ChatTCPDemo/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace ChatTCPDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHost = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblHost
+            // 
+            this.lblHost.AutoSize = true;
+            this.lblHost.Location = new System.Drawing.Point(12, 9);
+            this.lblHost.Name = "lblHost";
+            this.lblHost.Size = new System.Drawing.Size(49, 17);
+            this.lblHost.TabIndex = 0;
+            this.lblHost.Text = "Host : ";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(180, 9);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(42, 17);
+            this.lblPort.TabIndex = 1;
+            this.lblPort.Text = "Port :";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(338, 6);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(419, 6);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(64, 6);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(100, 22);
+            this.txtHost.TabIndex = 4;
+            this.txtHost.Text = "127.0.0.1";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(232, 6);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 22);
+            this.txtPort.TabIndex = 5;
+            this.txtPort.Text = "8888";
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(64, 52);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(536, 283);
+            this.txtStatus.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.lblHost);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHost;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtStatus;
+    }
+}
+
---
+++ Client/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.lblHost = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(59, 66);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(536, 82);
+            this.txtMessage.TabIndex = 12;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(227, 20);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 22);
+            this.txtPort.TabIndex = 11;
+            this.txtPort.Text = "8888";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(59, 20);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(100, 22);
+            this.txtHost.TabIndex = 10;
+            this.txtHost.Text = "127.0.0.1";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(333, 20);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 9;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(175, 23);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(42, 17);
+            this.lblPort.TabIndex = 8;
+            this.lblPort.Text = "Port :";
+            // 
+            // lblHost
+            // 
+            this.lblHost.AutoSize = true;
+            this.lblHost.Location = new System.Drawing.Point(7, 23);
+            this.lblHost.Name = "lblHost";
+            this.lblHost.Size = new System.Drawing.Size(49, 17);
+            this.lblHost.TabIndex = 7;
+            this.lblHost.Text = "Host : ";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(520, 154);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 13;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(59, 183);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(536, 196);
+            this.txtStatus.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.lblHost);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Label lblHost;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtStatus;
+    }
+}
+
